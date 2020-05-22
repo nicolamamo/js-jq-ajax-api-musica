8c9bbde6536749104b06559b30ecc5ef5ae97f51
nicolamamo <62040103+nicolamamo@users.noreply.github.com>
2020-05-22 17:43:45 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -3,27 +3,43 @@ $(document).ready(function() {
 
 
 
-
-function datiAjax() {
+// chiamata ajax
   $.ajax({
     url: "https://flynn.boolean.careers/exercises/api/array/music",
     method: "GET",
-    success: function(dati) {
-	      console.log(dati);
+    success: function(data) {
+        // recupero dischi restituiti dall api (array)
+        var dischi = data.response
+        // ciclo tutti gli elementi
+        for (var i = 0; i < dischi.length; i++) {
+        // recupero l' elemento corrente
+          var cd = dischi[i];
+// recuperole info ( .AUTORE POSTER TITOLO ANNO)
 
-	  dati.response.forEach(d => {
-			var album = $("#hb-template").html();
-			var card = Handlebars.compile(album);
-			var oggetti = {classname: 'cd', img: d.poster, title:d.title ,name:d.author ,genere:d.genere ,anno:d.year };
-			var albumArtista  = card(oggetti);
-			$("#albums").append(albumArtista);
-	  });
+
+        var titolo = cd.title;
+        var autore = cd.author;
+        var anno   = cd.year;
+        var immagine = cd.poster;
+
+        //preparo
+        var context = {
+        'poster' : immagine,
+        'title '    :titolo,
+        'name'     : autore,
+        'anno' : anno,
+    }
+
+// compilo
+          var source = $("#hb-template").html();
+          var template = Handlebars.compile(source);
+
+          var html = template(context);
+          $(".cds-container").append(html) // fine ciclo
+        }
     },
     error: function() {
         alert('errore')
     }
   });
-
-};
-datiAjax()
 });
